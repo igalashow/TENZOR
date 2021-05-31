1d4915bb9b6da77eae42ee358aabd395cdd9dcfd
Ilia Galashov <83612156+igalashow@users.noreply.github.com>
2021-05-31 22:53:19 +0300

    Delete config.py

--- tenzor_dev/config.py
+++
@@ -1,6 +0,0 @@
-LEN_LINE = 80 # длина строки в выходном файле
-RECOMMEND = True    # False - рекомендации выключены
-
-# Рекомендательные списки
-WHITE_LIST = ['meduza.io', 'zona.media', 'habr.com']
-BLACK_LIST = ['tass.ru', 'ria.ru', 'lenta.ru']
